80edb73899c4d6cc173bad733f94f31534eae127
tanayachavan2 <tchavan367@gmail.com>
2025-06-23 10:19:07 +0530

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Arr {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        display(arr);
+    }
+
+    private static void display(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.print(arr[i]);
+            } else {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
